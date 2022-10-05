f1e78bc5537dedc5806a2ac24126515f3e1966a6
yazidham <79407250+yazidham@users.noreply.github.com>
2022-10-05 11:16:05 +0200

    everthing is working fine rn

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kobe Bryant's cv</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -15,10 +15,11 @@
 <?php include("header.php") ?>
 <script src="script.js"></script>
     
-    <div class="picContainer">
-        <img src="assets\Kobe-Portrait.jpeg" alt="ProfilePic">
+    <div class="KobePics">
+        <img class="Portraitkobe" src="assets\image\Kobe-Portrait.jpeg" alt="Portraitkobe">
+        <img class="landkobe" src="assets\image\kobe-landscape.jpg" alt="landkobe">
     </div>
-    
+        
     <main id="main">
         <section id="about-me">
         
