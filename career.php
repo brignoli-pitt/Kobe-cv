ad03a99484a0b2dfb6d95f873ae340d9b362fb1b
Bixente <lasserrebixente@gmail.com>
2022-10-05 12:40:56 +0200

    premier jet bix

--- career.php
+++ career.php
@@ -6,21 +6,95 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV of Kobe Bryant</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/CSS/career.css">
 </head>
 
 <body>
-    <nav>
-        <h1 id="cv">Kobe Bryant</h1>
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="career.html">About us</a></li>
-            <li><a href="skills.html">Featured posts</a></li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-    </nav>
-    <footer>
-        © 2022 wilders
-    </footer>
+    <?php include("header.php") ?>
+
+    <div class="header">
+
+    </div>
+    <div class="titreheader">
+    <h1>Kobe Bryant's Career</h1>
+</div>
+    <section>
+        <article>
+        <h2 class="titre">Trophees<h2>
+            <div>
+                <p>Olympic Champion<br>
+                2008 2012 
+                </p>
+            </div>  
+            <div>             
+                <p>NBA Champion <br>
+                2000 2001 2002 2009 2010
+                </p>
+            </div>  
+            <div>                
+                <p>NBA Most Valuate Player <br>
+                2008
+            </p>
+            </div>
+            <div>
+                <p>Slam Dunk Contest Champion<br>
+                1997 
+                </p>
+            </div>    
+            <div>                
+                <p>NBA Final MVP <br>
+                2009 2010
+            </p>
+            </div> 
+            <div>
+                <p>Academy Award for Best Short Film (Animation) <br>
+            2018
+            </p>
+            </div>
+        </article>
+        <article>
+            <h2 class="titre">Achievements<h2>
+                <div>                
+                    <p>NBA All star <br>
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;<br>
+                        &starf;
+                        &starf;
+                        &starf;
+                        &starf;
+                         &starf;
+                         &starf;
+                         &starf;
+                         &starf;
+                         &starf;
+                         
+                    </p>
+                </div>  
+            <div>                
+                <p>2 times champion scoring</p>
+            </div>
+            <div>                
+                <p>81 points in a single match</p>
+            </div>  
+            <div>                
+                <p>4th highest scorer in NBA history (33.000+ pts)</p>
+            </div> 
+            <div>
+                <p>1996-2016 with Los Angeles Lakers <br>
+                    2007-2012 with Team USA
+                </p>
+            </div>   
+            <div>
+                <p>Number 8 and 24 retired by Los Angeles Lakers</p>
+            </div>     
+</section>
+<?php include ("footer.php")?>
 </body>
 </html>
